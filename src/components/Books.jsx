dd5621aa1de440abba6275cad2b4e1f5430a9770
Fahki Rohandi <fahkirohandi07@gmail.com>
2023-12-05 05:48:35 +0800

    Recommended Books in Welcome Page

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,105 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './components.css';
-import { Link } from 'react-router-dom/cjs/react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRecBook } from '../redux/actions/detailBookActions';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Books() {
+	const dispatch = useDispatch();
+	const books = useSelector((state) => state.detailBook.book);
+	const loading = useSelector((state) => state.detailBook.loading);
+
+	useEffect(() => {
+		dispatch(getRecBook());
+	}, [dispatch]);
+
+	if (loading) {
+		return <p>Loading...</p>;
+	}
+
+	if (!Array.isArray(books) || books.length === 0) {
+		return <p>Loading...</p>;
+	}
+
+	const sortedBooks = books.sort((a, b) => new Date(b.buku_id) - new Date(a.buku_id));
+
+	const recBooks = sortedBooks.slice(0, 5);
+
 	return (
 		<div id="card-books">
-			<div
-				className="card card-compact bg-base-100"
-				style={{ width: '184px' }}
-			>
-				<figure className="img-book">
-					<Link to="/detail-books">
-						<img
-							src="https://i.imgur.com/gzVaE2g.png"
-							alt="Laskar Pelangi"
-						/>
-					</Link>
-				</figure>
-				<div className="card-body-0">
-					<h2 className="card-title">Laskar Pelangi</h2>
-					<p>
-						by <a href="">Andrea Hinata</a> * 105x dibaca
-					</p>
+			{recBooks.map((book) => (
+				<div
+					key={book.buku_id}
+					className="card card-compact bg-base-100"
+					style={{ width: '152px' }}
+				>
+					<figure className="img-book">
+						<Link to={`/detail-books/${book.buku_id}`}>
+							<img
+								src={book.gambar}
+								alt={book.judul}
+								style={{ width: '100%', height: '250px', objectFit: 'cover' }}
+							/>
+						</Link>
+					</figure>
+					<div className="card-body-0">
+						<h2 className="card-title">{book.judul}</h2>
+						<p>
+							by <a href="">{book.penulis}</a>
+						</p>
+					</div>
 				</div>
-			</div>
-			<div
-				className="card card-compact bg-base-100"
-				style={{ width: '184px' }}
-			>
-				<figure className="img-book">
-					<Link to="/detail-books">
-						<img
-							src="https://i.imgur.com/gzVaE2g.png"
-							alt="Laskar Pelangi"
-						/>
-					</Link>
-				</figure>
-				<div className="card-body-0">
-					<h2 className="card-title">Laskar Pelangi</h2>
-					<p>
-						by <a href="">Andrea Hinata</a> * 105x dibaca
-					</p>
-				</div>
-			</div>
-			<div
-				className="card card-compact bg-base-100"
-				style={{ width: '184px' }}
-			>
-				<figure className="img-book">
-					<Link to="/detail-books">
-						<img
-							src="https://i.imgur.com/gzVaE2g.png"
-							alt="Laskar Pelangi"
-						/>
-					</Link>
-				</figure>
-				<div className="card-body-0">
-					<h2 className="card-title">Laskar Pelangi</h2>
-					<p>
-						by <a href="">Andrea Hinata</a> * 105x dibaca
-					</p>
-				</div>
-			</div>
-			<div
-				className="card card-compact bg-base-100"
-				style={{ width: '184px' }}
-			>
-				<figure className="img-book">
-					<Link to="/detail-books">
-						<img
-							src="https://i.imgur.com/gzVaE2g.png"
-							alt="Laskar Pelangi"
-						/>
-					</Link>
-				</figure>
-				<div className="card-body-0">
-					<h2 className="card-title">Laskar Pelangi</h2>
-					<p>
-						by <a href="">Andrea Hinata</a> * 105x dibaca
-					</p>
-				</div>
-			</div>
-			<div
-				className="card card-compact bg-base-100"
-				style={{ width: '184px' }}
-			>
-				<figure className="img-book">
-					<Link to="/detail-books">
-						<img
-							src="https://i.imgur.com/gzVaE2g.png"
-							alt="Laskar Pelangi"
-						/>
-					</Link>
-				</figure>
-				<div className="card-body-0">
-					<h2 className="card-title">Laskar Pelangi</h2>
-					<p>
-						by <a href="">Andrea Hinata</a> * 105x dibaca
-					</p>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 }
