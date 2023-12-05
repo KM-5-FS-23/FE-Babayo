26c925d070511b25b99a35a9835ebae37fc54122
Fahki Rohandi <fahkirohandi07@gmail.com>
2023-12-06 00:00:15 +0800

    fixing dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
-import VitePluginTailwind from 'vite-plugin-tailwind';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePluginTailwind()],
+	plugins: [react()],
 	css: {
 		postcss: {
 			plugins: [autoprefixer()],
