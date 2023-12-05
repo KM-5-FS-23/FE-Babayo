2d054510bdb85638ded8a47d72c16511566116a9
Fahki Rohandi <fahkirohandi07@gmail.com>
2023-12-05 23:35:43 +0800

    tailwind build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import VitePluginTailwind from 'vite-plugin-tailwind';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer(),
-      ],
-    },
-  },
-})
+	plugins: [react(), VitePluginTailwind()],
+	css: {
+		postcss: {
+			plugins: [autoprefixer()],
+		},
+	},
+});
